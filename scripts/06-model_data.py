40f44b015bbb720f269c155338e58da56ff51aae
Kevin Shen <kevinzshen3@gmail.com>
2024-12-01 14:18:50 -0500

    Update 06-model_data.py script to split dataset into train and test splits.

--- scripts/06-model_data.py
+++ scripts/06-model_data.py
@@ -9,7 +9,9 @@
 import pandas as pd
 import numpy as np
 from statsmodels.regression.mixed_linear_model import MixedLM
+from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
+
 
 np.random.seed(2620)
 
@@ -20,33 +22,35 @@ pd.set_option('display.expand_frame_repr', False)
 # Read data
 combined_data = pd.read_csv('../data/02-analysis_data/cleaned_sakura_data.csv')
 
-# # Convert flower_date and full_bloom_date into datetime format
-# combined_data['flower_date'] = pd.to_datetime(combined_data['flower_date'], errors='coerce')
-# combined_data['full_bloom_date'] = pd.to_datetime(combined_data['full_bloom_date'], errors='coerce')
+train_data, test_data = train_test_split(
+    combined_data,
+    test_size=0.2,
+    random_state=2620
+)
+
+# Save train and test datasets for later use
+train_data.to_csv('../data/02-analysis_data/train_test_data/train_data.csv', index=False)
+test_data.to_csv('../data/02-analysis_data/train_test_data/test_data.csv', index=False)
 
 
-def linear_mixed_effects_model(df):
+def linear_mixed_effects_model(train_df):
     """
     Fit mixed effects model with:
     - Fixed effects: flower_date, days_to_full_bloom, mean_temp_c, latitude, longitude
     - Random effects: station_id
     Args:
-        df: DataFrame containing the necessary columns
+        train_df: DataFrame containing the necessary columns
     Returns:
         Fitted mixed effects model
     """
     # Specify fixed effects
-    fixed_effects = sm.add_constant(df[['flower_doy', 'mean_temp_c', 'latitude', 'longitude']])
-
-    # # Create a simpler random effects structure
-    # random_slopes = df[['year']]  # Year as continuous variable
+    fixed_effects = sm.add_constant(train_df[['flower_doy', 'mean_temp_c', 'latitude', 'longitude']])
 
     # Fit the model
     model = MixedLM(
-        endog=df['full_bloom_doy'],
+        endog=train_df['full_bloom_doy'],
         exog=fixed_effects,
-        groups=df['station_id'],  # Primary random effect
-        # exog_re=random_slopes  # Secondary random effect
+        groups=train_df['station_id'],  # Primary random effect
     )
 
     result = model.fit()
@@ -59,7 +63,7 @@ def linear_mixed_effects_model(df):
 required_cols = ['full_bloom_doy', 'flower_doy', 'mean_temp_c', 'latitude', 'longitude', 'station_id']
 
 # Fit the model
-model_result = linear_mixed_effects_model(combined_data)
+model_result = linear_mixed_effects_model(train_data)
 
 # Print summary
 print(model_result.summary())
